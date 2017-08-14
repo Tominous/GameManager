de2621cb24ff6931cfc425a1a7f25c61c4d5856c
RapidTheNerd <george.marr.cobc@gmail.com>
2017-08-14 17:49:48 +0100

    should be hunky dorry

--- src/main/java/me/rtn/gamemanager/Main.java
+++ src/main/java/me/rtn/gamemanager/Main.java
@@ -3,6 +3,8 @@ package me.rtn.gamemanager;
 import me.rtn.gamemanager.data.DataHandler;
 import me.rtn.gamemanager.game.Game;
 import me.rtn.gamemanager.game.GamePlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,7 +27,7 @@ public final class Main extends JavaPlugin {
     private int gamesLimit = 0;
     private boolean isSingleServerMode = false;
 
-    private Map<Player, GamePlayer> playerGameMap = new HashMap<>();
+    private Map<Player, Game> playerGameMap = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -39,14 +41,14 @@ public final class Main extends JavaPlugin {
 
         this.isSingleServerMode = getConfig().getBoolean("single-server-mode");
 
-        if(this.isSingleServerMode){
+        if (this.isSingleServerMode) {
             gamesLimit = 1;
         } else {
             gamesLimit = getConfig().getInt("max-games");
         }
 
-        if(DataHandler.getDataHandler().getFileConfiguration().getConfigurationSection("games") != null){
-            for(String gameName : DataHandler.getDataHandler().getFileConfiguration().getConfigurationSection("games").getKeys(false)){
+        if (DataHandler.getDataHandler().getFileConfiguration().getConfigurationSection("games") != null) {
+            for (String gameName : DataHandler.getDataHandler().getFileConfiguration().getConfigurationSection("games").getKeys(false)) {
                 Game game = new Game(gameName);
                 //todo register game
             }
@@ -59,14 +61,84 @@ public final class Main extends JavaPlugin {
     public void onDisable() {
         instance = this;
 
-        for(Map.Entry<Player, GamePlayer> entry : playerGameMap.entrySet()){
-            try{
-                //todo lobby point
-            } catch(Exception e){
+        for (Map.Entry<Player, Game> entry : playerGameMap.entrySet()) {
+            try {
+                entry.getKey().teleport(getLobbyPoint());
+            } catch (Exception e) {
                 getLogger().info("Error: " + e);
             }
             entry.getKey().getInventory().clear();
             entry.getKey().getInventory().setArmorContents(new ItemStack[0]);
         }
+    }
+
+    private Location lobbyPoint = null;
+
+    public Location getLobbyPoint() {
+        if (lobbyPoint == null) {
+            int x = 0;
+            int y = 0;
+            int z = 0;
+            //probably should be doubles but fuck it
+            String world = "world";
+
+            try {
+                x = getInstance().getConfig().getInt("lobby-point.x");
+                y = getInstance().getConfig().getInt("lobby-point.y");
+                z = getInstance().getConfig().getInt("lobby-point.z");
+                world = getInstance().getConfig().getString("lobby-point.world");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            lobbyPoint = new Location(Bukkit.getWorld(world), x, y, z);
+        }
+        return lobbyPoint;
+    }
+
+    private boolean registerGame(Game game) {
+        if (games.size() == gamesLimit && gamesLimit != -1) {
+            return false;
+        }
+        games.add(game);
+        return true;
+    }
+
+    public Game getActiveGame(String gameName) {
+        for (Game game : games) {
+            if (game.getDisplayName().equalsIgnoreCase(gameName))
+                return game;
+        }
+        return null;
+    }
+
+    public void setGame(Player player, Game game){
+        if(game == null){
+            this.playerGameMap.remove(player);
+        } else {
+            this.playerGameMap.put(player, game);
+        }
+    }
+    public Game getGame(String gameName) {
+        for (Game game : games) {
+            if (game.getDisplayName().equalsIgnoreCase(gameName))
+                return game;
+        }
+        return null;
+    }
+
+    public Set<Game> getGames() {
+        return games;
+    }
+
+    public int getGamesLimit() {
+        return gamesLimit;
+    }
+
+    public boolean isSingleServerMode() {
+        return isSingleServerMode;
+    }
+
+    public Map<Player, Game> getPlayerGameMap() {
+        return playerGameMap;
     }
 }
