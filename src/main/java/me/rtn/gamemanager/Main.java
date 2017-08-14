f72ae3b37ad38f46dd94335cf0191ab1ccfaff9c
RapidTheNerd <george.marr.cobc@gmail.com>
2017-08-14 01:49:36 +0100

    okay enough for tonight

--- src/main/java/me/rtn/gamemanager/Main.java
+++ src/main/java/me/rtn/gamemanager/Main.java
@@ -1,6 +1,13 @@
 package me.rtn.gamemanager;
 
+import me.rtn.gamemanager.game.Game;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public final class Main extends JavaPlugin {
 
@@ -9,6 +16,12 @@ public final class Main extends JavaPlugin {
     public static Main getInstance() {
         return instance;
     }
+
+    private Set<Game> games = new HashSet<>();
+    private int gamesLimit = 0;
+    private boolean isSingleServerMode = false;
+
+    private Map<Player, Game> playerGameMap = new HashMap<>();
 
     @Override
     public void onEnable() {
---
+++ src/main/java/me/rtn/gamemanager/game/GamePlayer.java
@@ -0,0 +1,22 @@
+package me.rtn.gamemanager.game;
+
+/*
+ * GameManager 
+ * Created by George at 1:46 AM on 14-Aug-17  
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+public class GamePlayer {
+}
