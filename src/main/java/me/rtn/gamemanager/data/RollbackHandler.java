e7ffb29995cad1fa1954203ef97945a1cfee9481
RapidTheNerd <george.marr.cobc@gmail.com>
2017-08-14 01:44:23 +0100

    aids

--- src/main/java/me/rtn/gamemanager/data/RollbackHandler.java
+++ src/main/java/me/rtn/gamemanager/data/RollbackHandler.java
@@ -1,5 +1,11 @@
 package me.rtn.gamemanager.data;
 
+import me.rtn.gamemanager.Main;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.io.*;
+
 /*
  * GameManager 
  * Created by George at 1:37 AM on 14-Aug-17  
@@ -24,5 +30,68 @@ public class RollbackHandler {
 
     public static RollbackHandler getRollbackHandler() {
         return rollbackHandler;
+    }
+    public void rollback(World world){
+        for(Player player : world.getPlayers()){
+            //todo lobby point
+        }
+        Main.getInstance().getServer().unloadWorld(world, false);
+        String originalName = world.getName().split("_")[0];
+        rollBack(originalName);
+    }
+    public void rollBack(String worldName){
+        String rootDir = Main.getInstance().getServer().getWorldContainer().getAbsolutePath();
+
+        File srcFolder = new File(rootDir + "/" + worldName);
+        File destFolder = new File(rootDir + "/" + worldName + "_active");
+
+        delete(destFolder);
+
+        try{
+            copyFolder(srcFolder, destFolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void delete(File delete){
+        if(delete.isDirectory()){
+            String[] files = delete.list();
+
+            if(files != null){
+                for(String file : files){
+                    File toDelete = new File(file);
+                    delete(toDelete);
+                }
+            }
+        } else {
+            delete.delete();
+        }
+    }
+    private void copyFolder(File src, File dest) throws IOException {
+        if(src.isDirectory()){
+            if(!dest.exists()){
+                dest.mkdir();
+            }
+            String[] files = src.list();
+
+            if(files != null){
+                for(String file : files){
+                    File srcFile = new File(src, file);
+                    File destfile = new File(dest, file);
+                    copyFolder(srcFile, destfile);
+                }
+            }
+        } else {
+            InputStream in = new FileInputStream(src);
+            OutputStream out = new FileOutputStream(dest);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while((length = in.read(buffer)) > 0){
+                out.write(buffer, 0, length);
+            }
+            in.close();
+            out.close();
+        }
     }
 }
