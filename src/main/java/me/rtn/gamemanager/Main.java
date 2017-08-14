8a71b55d8b8f3a73472a4395b5c4b896fb0d2838
RapidTheNerd <george.marr.cobc@gmail.com>
2017-08-14 01:32:46 +0100

    added instance

--- src/main/java/me/rtn/gamemanager/Main.java
+++ src/main/java/me/rtn/gamemanager/Main.java
@@ -4,13 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    private static Main instance;
+
+    public static Main getInstance() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
+        instance = this;
 
     }
 
     @Override
     public void onDisable() {
-
+        instance = this;
     }
 }
