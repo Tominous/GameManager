38ca9c70f32b4fc2825eaff6877f9c3b96eba760
RapidTheNerd <george.marr.cobc@gmail.com>
2017-08-14 17:41:57 +0100

    hruurururururur

--- src/main/java/me/rtn/gamemanager/Main.java
+++ src/main/java/me/rtn/gamemanager/Main.java
@@ -1,10 +1,13 @@
 package me.rtn.gamemanager;
 
+import me.rtn.gamemanager.data.DataHandler;
 import me.rtn.gamemanager.game.Game;
 import me.rtn.gamemanager.game.GamePlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.xml.crypto.Data;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -28,10 +31,42 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         instance = this;
 
+        getConfig().options().copyDefaults(true);
+        getConfig().options().copyHeader(true);
+        saveDefaultConfig();
+
+        //todo commands
+
+        this.isSingleServerMode = getConfig().getBoolean("single-server-mode");
+
+        if(this.isSingleServerMode){
+            gamesLimit = 1;
+        } else {
+            gamesLimit = getConfig().getInt("max-games");
+        }
+
+        if(DataHandler.getDataHandler().getFileConfiguration().getConfigurationSection("games") != null){
+            for(String gameName : DataHandler.getDataHandler().getFileConfiguration().getConfigurationSection("games").getKeys(false)){
+                Game game = new Game(gameName);
+                //todo register game
+            }
+        } else {
+            getLogger().warning("[!] No games are currently running! [!]");
+        }
     }
 
     @Override
     public void onDisable() {
         instance = this;
+
+        for(Map.Entry<Player, GamePlayer> entry : playerGameMap.entrySet()){
+            try{
+                //todo lobby point
+            } catch(Exception e){
+                getLogger().info("Error: " + e);
+            }
+            entry.getKey().getInventory().clear();
+            entry.getKey().getInventory().setArmorContents(new ItemStack[0]);
+        }
     }
 }
